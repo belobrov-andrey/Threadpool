4b6e67de133e59290089ffe00587ad82b115f396
Daniel J. Hofmann <daniel@trvx.org>
2013-01-24 18:31:12 +0100

    initial adjustments

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ ThreadPool
 
 A simple C++11 Thread Pool implementation.
 
-The "legacy" directory contains a version that does not use std::future
-but a custom Result<T> type that has essentially the same functionalty.
+Improvements to the original version at https://github.com/progschj/ThreadPool
--- main.cpp
+++ main.cpp
@@ -1,29 +1,31 @@
+// workaround
+#define _GLIBCXX_USE_NANOSLEEP
+
 #include <iostream>
 #include <vector>
 #include <chrono>
 
-#include "ThreadPool.h"
+#include "Threadpool.hpp"
 
-int main()
-{
-    
+int main() {
     ThreadPool pool(4);
-    std::vector< std::future<int> > results;
 
-    for(int i = 0; i < 8; ++i) {
+    typedef std::vector<std::future<int>> rvec_t;
+    rvec_t results;
+
+    for(auto i = 0; i < 8; ++i) {
         results.push_back(
             pool.enqueue<int>([i] {
                 std::cout << "hello " << i << std::endl;
                 std::this_thread::sleep_for(std::chrono::seconds(1));
                 std::cout << "world " << i << std::endl;
-                return i*i;
+
+                return i * i;
             })
         );
-    }   
-    
-    for(size_t i = 0;i<results.size();++i)
+    }
+
+    for(rvec_t::size_type i = 0; i < results.size(); ++i)
         std::cout << results[i].get() << ' ';
     std::cout << std::endl;
-    
-    return 0;
 }
